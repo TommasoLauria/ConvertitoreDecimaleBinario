e469883ea2519cb4f7dbe9ac68416c316dac6219
tommaso lauria <tommaso.lauria9@gmail.com>
2020-10-14 08:56:36 +0200

    Add project files.

---
+++ ConvertitoreDecimaleBinario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConvertitoreDecimaleBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resto, decimale;
+            string numbinario="";
+            Console.WriteLine("inserisci il numero che vuoi convertire");
+            decimale = Convert.ToInt32(Console.ReadLine());
+            while(decimale>0)
+            {
+                resto = decimale % 2;
+                decimale = decimale / 2;
+                numbinario = Convert.ToString(resto) + numbinario;
+            }
+            Console.WriteLine($"il numero binario è {numbinario} ");
+        }
+    }
+}
