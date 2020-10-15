0812263e50f60c191877e3ecb67e4c8a1b731e6f
tommaso lauria <tommaso.lauria9@gmail.com>
2020-10-15 09:22:06 +0200

    aggiornato2

--- ConvertitoreDecimaleBinario/Program.cs
+++ ConvertitoreDecimaleBinario/Program.cs
@@ -6,15 +6,21 @@ namespace ConvertitoreDecimaleBinario
     {
         static void Main(string[] args)
         {
-            int resto, decimale;
-            string numbinario="";
-            Console.WriteLine("inserisci il numero che vuoi convertire");
+            int resto, decimale, ottale, restoottale;
+            string numbinario = "", nbottale = "";
+            Console.WriteLine("inserisci il numero decimale che vuoi convertire");
             decimale = Convert.ToInt32(Console.ReadLine());
-            while(decimale>0)
+            Console.WriteLine("inserisci il numero ottale che vuoi convertire");
+            ottale = Convert.ToInt32(Console.ReadLine());
+            while (decimale > 0)
             {
                 resto = decimale % 2;
                 decimale = decimale / 2;
                 numbinario = Convert.ToString(resto) + numbinario;
+                restoottale = ottale % 2;
+                ottale = ottale / 2;
+                nbottale=Convert.ToString
+
             }
             Console.WriteLine($"il numero binario  è  {numbinario} ");
         }
